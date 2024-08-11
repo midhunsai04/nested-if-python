9f8fede9e2b3aaba2e29296ab53ae0bf9c582235
Karuru Midhun Sai <164469674+midhunsai04@users.noreply.github.com>
2024-08-11 16:55:03 +0530

    Add files via upload

---
+++ fake fan(nested-if).py
@@ -0,0 +1,12 @@
+myname=input("what is your favourite marvel character? :")
+if(myname=="ironman"):
+  print("that's why he is the goat")
+  realname=input("what is his real name? :")
+  if(realname=="tony stark"):
+    print("hell yeah")
+  else:
+    print("you are not correct")
+elif(myname=="thor"):
+  print("you are good")
+else:
+  print("see fake fan")
